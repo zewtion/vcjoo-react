edb05d64d0eaaca4f5246909c862c3161cf7f6de
AYAN <zewtion@gmail.com>
2018-07-24 05:24:04 +0900

    add json module

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-
+import data from './data.json';
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -10,6 +10,7 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <h2>{data.danjis[0].name}</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
